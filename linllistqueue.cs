468b46c5eb6c5e87ae666fe8013b71522da39b5d
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 15:41:58 +0530

    create queue code

---
+++ linllistqueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace stackandqueue
+{
+    internal class linllistqueue
+    {
+        public node head = null;
+        public void Enqueue(int data)
+        {
+            node node = new node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} inserted into Queue");
+        }
+        public void Display()
+        {
+            node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
