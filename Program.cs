421020a71abc35fbd78e56eabc8cca494d359954
NikitaGitAC <126748418+NikitaGitAC@users.noreply.github.com>
2023-04-01 14:45:53 +0530

    Welcome page

--- Program.cs
+++ Program.cs
@@ -7,11 +7,7 @@ namespace LinkedListAsmnt
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack Problem");
-            linkliststack linkedListStack = new linkliststack();
-            linkedListStack.Push(6);
-            linkedListStack.Push(60);
-            linkedListStack.Push(90);
-            linkedListStack.Display();
+            
         }
     }
 }
