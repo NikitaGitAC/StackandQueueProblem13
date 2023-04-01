88e0e3fd4da785b9b55afbb78b20f12824714a7f
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 14:38:09 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using stackandqueue;
+
+namespace LinkedListAsmnt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack Problem");
+            linkliststack linkedListStack = new linkliststack();
+            linkedListStack.Push(6);
+            linkedListStack.Push(60);
+            linkedListStack.Push(90);
+            linkedListStack.Display();
+        }
+    }
+}
