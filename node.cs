f4e35e560b6bf727b998f3e8b0502867386ad2f7
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 15:10:43 +0530

    assigning node

---
+++ node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackandqueue
+{
+        public class node
+        {
+            public int data;
+            public node next;
+
+            public node(int data)
+            {
+                this.data = data;
+            }
+
+        }
+}
