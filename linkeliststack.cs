b511bb179f5ae0c782de68c54b77cba0fe09b0ca
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 15:13:11 +0530

    push value in stack

---
+++ linkeliststack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace stackandqueue
+{
+    public class linkliststack
+    {
+        private node top;
+        public linkliststack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            node node = new node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
