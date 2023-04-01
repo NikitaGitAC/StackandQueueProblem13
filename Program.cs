926e7173bda54f2b0eccbe4e4f0679f5b3b9d9dd
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 15:56:01 +0530

    inserting and deletion in queue

--- Program.cs
+++ Program.cs
@@ -11,7 +11,12 @@ namespace LinkedListAsmnt
             obj.Enqueue(56);
             obj.Enqueue(30);
             obj.Enqueue(70);
+            Console.WriteLine("Element stored in queue is:-");
             obj.Display();
+            Console.WriteLine("After Dequeue operation:");
+            obj.Dequeue();
+            Console.WriteLine("Element stored in queue is:-");
+            obj.Display(); 
 
         }
     }
